96082fe470626c2085f64c790f0f0813df8424a1
Carlos16d <91160548+Carlos16d@users.noreply.github.com>
2026-05-31 19:20:32 -0300

    Update TorrentBridge.java

--- app/src/main/java/com/seunome/meuapp/TorrentBridge.java
+++ app/src/main/java/com/seunome/meuapp/TorrentBridge.java
@@ -1,55 +1,178 @@
-package com.seunome.meuapp;
+package com.seuapp;
 
 import android.content.Context;
+import com.frostwire.jlibtorrent.*;
+import com.frostwire.jlibtorrent.alerts.*;
+import com.frostwire.jlibtorrent.swig.settings_pack;
+
 import java.io.File;
 
-public class TorrentBridge {
+public class TorrentServer {
+    private SessionManager session;
+    private TorrentHandle torrentHandle;
+    private StreamServer streamServer;
+    private float progress = 0;
+    private int peerCount = 0;
+    private long downloadSpeed = 0;
+    private File currentFile;
     
-    private Context context;
-    private TorrentListener listener;
-    
-    static {
-        System.loadLibrary("torrent_bridge");
+    public TorrentServer(Context context, StreamServer streamServer) {
+        this.streamServer = streamServer;
+        initSession();
     }
     
-    public interface TorrentListener {
-        void onProgress(float progress, int downloadSpeed, int peers);
-        void onReady(String videoPath);
-        void onError(String error);
-        void onStatus(String status);
-    }
-    
-    public TorrentBridge(Context context, TorrentListener listener) {
-        this.context = context;
-        this.listener = listener;
+    private void initSession() {
+        SettingsPack sp = new SettingsPack();
+        sp.setBoolean(settings_pack.bool_types.enable_dht, true);
+        sp.setInteger(settings_pack.int_types.dht_announce_interval, 300);
+        sp.setBoolean(settings_pack.bool_types.enable_lsd, true);
+        sp.setBoolean(settings_pack.bool_types.enable_upnp, true);
+        sp.setBoolean(settings_pack.bool_types.enable_natpmp, true);
+        sp.setInteger(settings_pack.int_types.alert_mask, 
+            AlertCategory.ALL.getSwig());
+        sp.setInteger(settings_pack.int_types.connections_limit, 200);
+        sp.setInteger(settings_pack.int_types.download_rate_limit, 0);
+        sp.setInteger(settings_pack.int_types.upload_rate_limit, 0);
+        sp.setBoolean(settings_pack.bool_types.announce_to_all_trackers, true);
+        sp.setBoolean(settings_pack.bool_types.announce_to_all_tiers, true);
         
-        String savePath = context.getExternalFilesDir(null).getAbsolutePath() + "/torrents/";
-        new File(savePath).mkdirs();
+        session = new SessionManager(sp);
+        session.start();
         
-        initEngine(savePath);
+        // Adiciona DHT routers
+        session.addDhtNode(new Pair<>("router.bittorrent.com", 6881));
+        session.addDhtNode(new Pair<>("dht.transmissionbt.com", 6881));
+        session.addDhtNode(new Pair<>("dht.libtorrent.org", 25401));
+        
+        // Adiciona trackers UDP padrão
+        addDefaultTrackers();
+        
+        // Thread para processar alertas
+        startAlertListener();
     }
     
-    // Métodos nativos
-    private native void initEngine(String savePath);
-    public native void addMagnet(String magnetUri);
-    public native void pause();
-    public native void resume();
-    public native void destroy();
-    
-    // Callbacks do C++
-    private void onProgress(float progress, int downloadSpeed, int peers) {
-        if (listener != null) listener.onProgress(progress, downloadSpeed, peers);
+    private void addDefaultTrackers() {
+        String[][] trackers = {
+            {"udp://tracker.opentrackr.org:1337"},
+            {"udp://tracker.openbittorrent.com:6969"},
+            {"udp://open.stealth.si:80"},
+            {"udp://tracker.torrent.eu.org:451"},
+            {"udp://tracker.moeking.me:6969"},
+            {"udp://explodie.org:6969"},
+            {"udp://tracker.cyberia.is:6969"},
+            {"udp://9.rarbg.to:2710"},
+            {"udp://tracker.coppersurfer.tk:6969"}
+        };
+        
+        for (String[] tracker : trackers) {
+            session.addTrackers(new Pair<>(tracker[0], 0));
+        }
     }
     
-    private void onReady(String videoPath) {
-        if (listener != null) listener.onReady(videoPath);
+    public void playTorrent(String magnetURI) {
+        playTorrent(magnetURI, 0);
     }
     
-    private void onError(String error) {
-        if (listener != null) listener.onError(error);
+    public void playTorrent(String magnetURI, int fileIndex) {
+        try {
+            // Remove torrent anterior se existir
+            if (torrentHandle != null) {
+                session.remove(torrentHandle);
+            }
+            
+            // Cria diretório de cache
+            File cacheDir = new File(
+                android.os.Environment.getExternalStorageDirectory(), 
+                "MeuAppCache"
+            );
+            cacheDir.mkdirs();
+            
+            // Adiciona o torrent
+            byte[] data = magnetURI.getBytes();
+            session.download(data, cacheDir, null, null, null);
+            
+            // Aguarda metadados
+            new Thread(() -> {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {}
+                
+                TorrentHandle[] handles = session.find(magnetURI);
+                if (handles.length > 0) {
+                    torrentHandle = handles[0];
+                    
+                    // Configura download sequencial
+                    torrentHandle.setSequentialDownload(true);
+                    
+                    // Define prioridade apenas para o arquivo de vídeo
+                    TorrentInfo ti = torrentHandle.torrentFile();
+                    if (ti != null && ti.numFiles() > fileIndex) {
+                        // Prioridade alta para o arquivo selecionado
+                        int[] filePriorities = new int[ti.numFiles()];
+                        for (int i = 0; i < filePriorities.length; i++) {
+                            filePriorities[i] = (i == fileIndex) ? 7 : 0;
+                        }
+                        torrentHandle.prioritizeFiles(Priority.array(filePriorities));
+                        
+                        // Configura peças para download sequencial
+                        long fileSize = ti.files().fileSize(fileIndex);
+                        currentFile = new File(cacheDir, ti.files().filePath(fileIndex));
+                    }
+                    
+                    // Atualiza o StreamServer com o arquivo
+                    streamServer.setTorrentHandle(torrentHandle, fileIndex, cacheDir);
+                }
+            }).start();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
-    private void onStatus(String status) {
-        if (listener != null) listener.onStatus(status);
+    private void startAlertListener() {
+        new Thread(() -> {
+            while (session != null) {
+                Alert[] alerts = session.alerts();
+                for (Alert alert : alerts) {
+                    switch (alert.type()) {
+                        case TORRENT_UPDATE:
+                            updateStats();
+                            break;
+                        case TORRENT_FINISHED:
+                            System.out.println("Download completo!");
+                            break;
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {}
+            }
+        }).start();
+    }
+    
+    private void updateStats() {
+        if (torrentHandle != null) {
+            TorrentStatus status = torrentHandle.status();
+            progress = status.progress() * 100;
+            peerCount = status.numPeers();
+            downloadSpeed = status.downloadRate();
+        }
+    }
+    
+    public float getProgress() { return progress; }
+    public int getPeerCount() { return peerCount; }
+    public String getDownloadSpeed() {
+        if (downloadSpeed > 1048576)
+            return String.format("%.1f MB/s", downloadSpeed / 1048576.0);
+        else if (downloadSpeed > 1024)
+            return String.format("%.1f KB/s", downloadSpeed / 1024.0);
+        else
+            return downloadSpeed + " B/s";
+    }
+    
+    public void stop() {
+        if (session != null) {
+            session.stop();
+        }
     }
 }
