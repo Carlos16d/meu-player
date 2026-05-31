746b123c0846be17c48c0adb68cb0d5fb8f6177f
Carlos16d <91160548+Carlos16d@users.noreply.github.com>
2026-05-31 19:49:19 -0300

    Update  TorrentServer.java

--- app/src/main/java/com/seuapp/ TorrentServer.java
+++ app/src/main/java/com/seuapp/ TorrentServer.java
@@ -1,9 +1,16 @@
 package com.seuapp;
 
 import android.content.Context;
-import com.frostwire.jlibtorrent.*;
-import com.frostwire.jlibtorrent.alerts.*;
-import com.frostwire.jlibtorrent.swig.settings_pack;
+
+import org.libtorrent4j.AlertListener;
+import org.libtorrent4j.SessionManager;
+import org.libtorrent4j.SettingsPack;
+import org.libtorrent4j.TorrentHandle;
+import org.libtorrent4j.TorrentInfo;
+import org.libtorrent4j.TorrentStatus;
+import org.libtorrent4j.alerts.Alert;
+import org.libtorrent4j.alerts.AlertType;
+import org.libtorrent4j.swig.*;
 
 import java.io.File;
 
@@ -22,51 +29,31 @@ public class TorrentServer {
     }
     
     private void initSession() {
-        SettingsPack sp = new SettingsPack();
-        sp.setBoolean(settings_pack.bool_types.enable_dht, true);
-        sp.setInteger(settings_pack.int_types.dht_announce_interval, 300);
-        sp.setBoolean(settings_pack.bool_types.enable_lsd, true);
-        sp.setBoolean(settings_pack.bool_types.enable_upnp, true);
-        sp.setBoolean(settings_pack.bool_types.enable_natpmp, true);
-        sp.setInteger(settings_pack.int_types.alert_mask, 
-            AlertCategory.ALL.getSwig());
-        sp.setInteger(settings_pack.int_types.connections_limit, 200);
-        sp.setInteger(settings_pack.int_types.download_rate_limit, 0);
-        sp.setInteger(settings_pack.int_types.upload_rate_limit, 0);
-        sp.setBoolean(settings_pack.bool_types.announce_to_all_trackers, true);
-        sp.setBoolean(settings_pack.bool_types.announce_to_all_tiers, true);
+        // Configurações para UDP, DHT e trackers
+        settings_pack sp = new settings_pack();
+        sp.set_bool(settings_pack.bool_types.enable_dht, true);
+        sp.set_int(settings_pack.int_types.dht_announce_interval, 300);
+        sp.set_bool(settings_pack.bool_types.enable_lsd, true);
+        sp.set_bool(settings_pack.bool_types.enable_upnp, true);
+        sp.set_bool(settings_pack.bool_types.enable_natpmp, true);
+        sp.set_int(settings_pack.int_types.alert_mask, 
+            alert.category_t.all_categories.swigValue());
+        sp.set_int(settings_pack.int_types.connections_limit, 200);
+        sp.set_int(settings_pack.int_types.download_rate_limit, 0);
+        sp.set_int(settings_pack.int_types.upload_rate_limit, 0);
+        sp.set_bool(settings_pack.bool_types.announce_to_all_trackers, true);
+        sp.set_bool(settings_pack.bool_types.announce_to_all_tiers, true);
         
-        session = new SessionManager(sp);
-        session.start();
+        SessionManager.setParams(sp);
+        session = new SessionManager();
         
         // Adiciona DHT routers
-        session.addDhtNode(new Pair<>("router.bittorrent.com", 6881));
-        session.addDhtNode(new Pair<>("dht.transmissionbt.com", 6881));
-        session.addDhtNode(new Pair<>("dht.libtorrent.org", 25401));
-        
-        // Adiciona trackers UDP padrão
-        addDefaultTrackers();
+        session.addDhtNode("router.bittorrent.com", 6881);
+        session.addDhtNode("dht.transmissionbt.com", 6881);
+        session.addDhtNode("dht.libtorrent.org", 25401);
         
         // Thread para processar alertas
         startAlertListener();
-    }
-    
-    private void addDefaultTrackers() {
-        String[][] trackers = {
-            {"udp://tracker.opentrackr.org:1337"},
-            {"udp://tracker.openbittorrent.com:6969"},
-            {"udp://open.stealth.si:80"},
-            {"udp://tracker.torrent.eu.org:451"},
-            {"udp://tracker.moeking.me:6969"},
-            {"udp://explodie.org:6969"},
-            {"udp://tracker.cyberia.is:6969"},
-            {"udp://9.rarbg.to:2710"},
-            {"udp://tracker.coppersurfer.tk:6969"}
-        };
-        
-        for (String[] tracker : trackers) {
-            session.addTrackers(new Pair<>(tracker[0], 0));
-        }
     }
     
     public void playTorrent(String magnetURI) {
@@ -75,7 +62,7 @@ public class TorrentServer {
     
     public void playTorrent(String magnetURI, int fileIndex) {
         try {
-            // Remove torrent anterior se existir
+            // Remove torrent anterior
             if (torrentHandle != null) {
                 session.remove(torrentHandle);
             }
@@ -88,39 +75,40 @@ public class TorrentServer {
             cacheDir.mkdirs();
             
             // Adiciona o torrent
-            byte[] data = magnetURI.getBytes();
-            session.download(data, cacheDir, null, null, null);
+            add_torrent_params params = new add_torrent_params();
+            params.set_url(magnetURI);
+            params.set_save_path(cacheDir.getAbsolutePath());
             
-            // Aguarda metadados
+            // Força download sequencial
+            params.set_flags(add_torrent_params.flags_t.flag_sequential_download.swigValue());
+            
+            session.download(params);
+            
+            // Aguarda metadados e configura
             new Thread(() -> {
                 try {
                     Thread.sleep(3000);
                 } catch (Exception e) {}
                 
-                TorrentHandle[] handles = session.find(magnetURI);
-                if (handles.length > 0) {
-                    torrentHandle = handles[0];
-                    
-                    // Configura download sequencial
-                    torrentHandle.setSequentialDownload(true);
-                    
-                    // Define prioridade apenas para o arquivo de vídeo
+                // Procura o torrent adicionado
+                torrentHandle = session.find(magnetURI);
+                
+                if (torrentHandle != null) {
                     TorrentInfo ti = torrentHandle.torrentFile();
                     if (ti != null && ti.numFiles() > fileIndex) {
-                        // Prioridade alta para o arquivo selecionado
+                        // Configura prioridade do arquivo
                         int[] filePriorities = new int[ti.numFiles()];
                         for (int i = 0; i < filePriorities.length; i++) {
                             filePriorities[i] = (i == fileIndex) ? 7 : 0;
                         }
-                        torrentHandle.prioritizeFiles(Priority.array(filePriorities));
+                        torrentHandle.prioritizeFiles(filePriorities);
                         
-                        // Configura peças para download sequencial
-                        long fileSize = ti.files().fileSize(fileIndex);
-                        currentFile = new File(cacheDir, ti.files().filePath(fileIndex));
+                        // Configura peças sequenciais
+                        torrentHandle.setSequentialDownload(true);
+                        
+                        // Atualiza o StreamServer
+                        streamServer.setTorrentHandle(torrentHandle, fileIndex, cacheDir);
                     }
-                    
-                    // Atualiza o StreamServer com o arquivo
-                    streamServer.setTorrentHandle(torrentHandle, fileIndex, cacheDir);
                 }
             }).start();
             
@@ -132,26 +120,20 @@ public class TorrentServer {
     private void startAlertListener() {
         new Thread(() -> {
             while (session != null) {
-                Alert[] alerts = session.alerts();
-                for (Alert alert : alerts) {
-                    switch (alert.type()) {
-                        case TORRENT_UPDATE:
-                            updateStats();
-                            break;
-                        case TORRENT_FINISHED:
-                            System.out.println("Download completo!");
-                            break;
-                    }
-                }
                 try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {}
+                    session.waitForAlerts(1000);
+                    updateStats();
+                } catch (Exception e) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ie) {}
+                }
             }
         }).start();
     }
     
     private void updateStats() {
-        if (torrentHandle != null) {
+        if (torrentHandle != null && torrentHandle.isValid()) {
             TorrentStatus status = torrentHandle.status();
             progress = status.progress() * 100;
             peerCount = status.numPeers();
@@ -172,7 +154,7 @@ public class TorrentServer {
     
     public void stop() {
         if (session != null) {
-            session.stop();
+            session.close();
         }
     }
 }
