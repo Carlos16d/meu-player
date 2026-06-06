99f01b8789243fa602c59f17461cacdf5df6b14b
Carlos16d <91160548+Carlos16d@users.noreply.github.com>
2026-06-06 16:41:30 -0300

    Atualizar o ExoPlayerManager.java

--- app/src/main/java/com/meuapp/player/player/ExoPlayerManager.java
+++ app/src/main/java/com/meuapp/player/player/ExoPlayerManager.java
@@ -6,7 +6,6 @@ import android.view.View;
 
 import com.google.android.exoplayer2.*;
 import com.google.android.exoplayer2.source.*;
-import com.google.android.exoplayer2.trackselection.*;
 import com.google.android.exoplayer2.ui.*;
 import com.google.android.exoplayer2.upstream.*;
 import com.google.android.exoplayer2.util.*;
@@ -26,57 +25,22 @@ public class ExoPlayerManager {
     public ExoPlayerManager(PlayerView view, SimpleExoPlayer p) {
         this.playerView = view;
         this.player = p;
-        
-        Log.d(TAG, "Inicializando ExoPlayer...");
-        
-        // Configura controles de áudio/legendas
         playerView.setPlayer(player);
         playerView.setUseController(true);
         playerView.setControllerShowTimeoutMs(5000);
         playerView.setKeepScreenOn(true);
         playerView.setControllerAutoShow(true);
         
-        // Mostra botões de áudio e legendas
-        playerView.setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING);
-        
-        setupPlayerListener();
-        Log.d(TAG, "ExoPlayer configurado");
-    }
-    
-    private void setupPlayerListener() {
         player.addListener(new Player.Listener() {
             @Override
             public void onTracksChanged(Tracks tracks) {
                 int audioTracks = 0;
                 int subtitleTracks = 0;
                 
-                Log.d(TAG, "=== TRACKS DISPONÍVEIS ===");
-                Log.d(TAG, "Grupos: " + tracks.getGroups().size());
-                
                 for (Tracks.Group group : tracks.getGroups()) {
-                    int type = group.getMediaTrackGroup().type;
-                    String typeName = type == C.TRACK_TYPE_AUDIO ? "ÁUDIO" : 
-                                     type == C.TRACK_TYPE_TEXT ? "LEGENDA" : 
-                                     type == C.TRACK_TYPE_VIDEO ? "VÍDEO" : "OUTRO";
-                    
-                    Log.d(TAG, "Grupo " + typeName + ": " + group.length + " faixas");
-                    
-                    for (int i = 0; i < group.length; i++) {
-                        Format format = group.getTrackFormat(i);
-                        Log.d(TAG, "  Faixa " + i + ": " + format.language + 
-                              " - " + format.label + 
-                              " - " + format.sampleMimeType);
-                    }
-                    
-                    if (type == C.TRACK_TYPE_AUDIO) {
-                        audioTracks += group.length;
-                    }
-                    if (type == C.TRACK_TYPE_TEXT) {
-                        subtitleTracks += group.length;
-                    }
+                    if (group.getMediaTrackGroup().type == C.TRACK_TYPE_AUDIO) audioTracks += group.length;
+                    if (group.getMediaTrackGroup().type == C.TRACK_TYPE_TEXT) subtitleTracks += group.length;
                 }
-                
-                Log.d(TAG, "Total áudio: " + audioTracks + ", legendas: " + subtitleTracks);
                 
                 if (playerListener != null) {
                     playerListener.onTracksAvailable(audioTracks, subtitleTracks);
@@ -85,16 +49,6 @@ public class ExoPlayerManager {
             
             @Override
             public void onPlaybackStateChanged(int state) {
-                String stateName;
-                switch (state) {
-                    case Player.STATE_IDLE: stateName = "IDLE"; break;
-                    case Player.STATE_BUFFERING: stateName = "BUFFERING"; break;
-                    case Player.STATE_READY: stateName = "READY"; break;
-                    case Player.STATE_ENDED: stateName = "ENDED"; break;
-                    default: stateName = "UNKNOWN"; break;
-                }
-                Log.d(TAG, "Estado do player: " + stateName);
-                
                 if (playerListener != null) {
                     playerListener.onBuffering(state == Player.STATE_BUFFERING);
                 }
@@ -102,74 +56,34 @@ public class ExoPlayerManager {
             
             @Override
             public void onPlayerError(PlaybackException error) {
-                Log.e(TAG, "ERRO NO PLAYER: " + error.getMessage(), error);
                 if (playerListener != null) {
                     playerListener.onError(error.getMessage());
                 }
-            }
-            
-            @Override
-            public void onPositionDiscontinuity(Player.PositionInfo oldPos, 
-                                                 Player.PositionInfo newPos, 
-                                                 int reason) {
-                Log.d(TAG, "Seek: " + oldPos.positionMs + " -> " + newPos.positionMs + " (reason: " + reason + ")");
             }
         });
     }
     
     public void play(String url) {
-        Log.d(TAG, "Iniciando reprodução: " + url);
-        
         Uri videoUri = Uri.parse(url);
-        
-        DataSource.Factory dataSourceFactory = new DefaultHttpDataSource.Factory()
+        DataSource.Factory factory = new DefaultHttpDataSource.Factory()
             .setConnectTimeoutMs(5000)
-            .setReadTimeoutMs(15000)
-            .setAllowCrossProtocolRedirects(true);
-        
-        ProgressiveMediaSource.Factory mediaSourceFactory = 
-            new ProgressiveMediaSource.Factory(dataSourceFactory);
-        
-        MediaItem mediaItem = new MediaItem.Builder()
-            .setUri(videoUri)
-            .build();
-        
-        MediaSource mediaSource = mediaSourceFactory.createMediaSource(mediaItem);
-        
-        player.setMediaSource(mediaSource);
+            .setReadTimeoutMs(15000);
+        ProgressiveMediaSource.Factory mediaFactory = new ProgressiveMediaSource.Factory(factory);
+        MediaSource source = mediaFactory.createMediaSource(MediaItem.fromUri(videoUri));
+        player.setMediaSource(source);
         player.prepare();
         player.setPlayWhenReady(true);
-        
-        Log.d(TAG, "Reprodução iniciada");
     }
     
     public void stop() {
-        Log.d(TAG, "Parando player");
-        if (player != null) {
-            player.stop();
-            player.clearMediaItems();
-        }
+        if (player != null) { player.stop(); player.clearMediaItems(); }
     }
     
     public void release() {
-        Log.d(TAG, "Liberando player");
-        if (player != null) {
-            player.release();
-            player = null;
-        }
+        if (player != null) { player.release(); player = null; }
     }
     
-    public void setPlayerListener(PlayerListener listener) {
-        this.playerListener = listener;
-    }
-    
-    public void show() { 
-        Log.d(TAG, "Mostrando player");
-        playerView.setVisibility(View.VISIBLE); 
-    }
-    
-    public void hide() { 
-        Log.d(TAG, "Escondendo player");
-        playerView.setVisibility(View.GONE); 
-    }
+    public void setPlayerListener(PlayerListener listener) { this.playerListener = listener; }
+    public void show() { playerView.setVisibility(View.VISIBLE); }
+    public void hide() { playerView.setVisibility(View.GONE); }
 }
