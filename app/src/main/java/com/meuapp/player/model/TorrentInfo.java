a1f4a480784b342eae5cb7dcc2488d8aa397250f
Carlos16d <91160548+Carlos16d@users.noreply.github.com>
2026-06-06 11:52:25 -0300

    Update TorrentInfo.java

--- app/src/main/java/com/meuapp/player/model/TorrentInfo.java
+++ app/src/main/java/com/meuapp/player/model/TorrentInfo.java
@@ -7,22 +7,4 @@ public class TorrentInfo {
     public int peers;
     public int seeds;
     public int progress;
-    public String fileName;
-    public long fileSize;
-    
-    public String getDownloadedMB() {
-        return String.format("%.1f MB", downloaded / 1048576.0);
-    }
-    
-    public String getTotalMB() {
-        return String.format("%.1f MB", total / 1048576.0);
-    }
-    
-    public String getSpeedKB() {
-        return (speed / 1024) + " KB/s";
-    }
-    
-    public String getPeersInfo() {
-        return peers + " peers (" + seeds + " seeds)";
-    }
 }
