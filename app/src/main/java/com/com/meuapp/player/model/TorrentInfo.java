b42757252d4abf1bafecc3584563026bca944946
Carlos16d <91160548+Carlos16d@users.noreply.github.com>
2026-06-10 18:09:26 -0300

    Create TorrentInfo.java

---
+++ app/src/main/java/com/com/meuapp/player/model/TorrentInfo.java
@@ -0,0 +1,10 @@
+package com.meuapp.player.model;
+
+public class TorrentInfo {
+    public long downloaded;
+    public long total;
+    public int speed;
+    public int peers;
+    public int seeds;
+    public int progress;
+}
